db7739e8de36ea7bb771903fa6402f3c52f50c9e
Tylerdurden230 <75187191+TylerDurden230@users.noreply.github.com>
2022-09-02 11:31:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 let cnt = document.getElementById("cnt");
 let cnt2 = document.getElementById("cnt2");
 let value = 0;
+let rating = document.getElementById("rating");
 
 function setActive(btn){
     value = btn;
 }
 
 function submit() {
-    if  (!value)
-        console.log("Select a value please")
-    else {
+    if  (value != 0) {
+        rating.innerHTML = `You selected ${value} out of 5`;
         cnt.style.display = "none";
         cnt2.style.display = "flex";
-        console.log(`You selected ${value} out of 5`);
     }
 }
-
